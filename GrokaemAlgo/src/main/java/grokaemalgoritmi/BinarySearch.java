8fb30a021ee44ffddd79b699a38cdbb8317347ea
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-03-08 17:25:47 +0300

    "Настройка"

---
+++ GrokaemAlgo/src/main/java/grokaemalgoritmi/BinarySearch.java
@@ -0,0 +1,28 @@
+package grokaemalgoritmi;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+       int res = search(new int[]{4, 6, 7, 10, 15}, 4);
+       if (res >= 0) {
+           System.out.println("Значение в списке расположена в позиции " + res);
+       } else  {
+           System.out.println("Значение не найдено");
+       }
+    }
+    public static int search(int[]mas, int value) {
+        int low = 0;
+        int high = mas.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+           int guess = mas[mid];
+            if (guess == value) {
+                return mid;
+            } else if (guess < value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
