fbed1370aed4a977ddcc687864a8b329f44eea67
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-04-16 12:38:51 +0300

    Глава 2. Содержательные имена.

--- CleanCode/src/main/java/glava2/soderzhatelnieimena/GuessStatisticsMessage.java
+++ CleanCode/src/main/java/glava2/soderzhatelnieimena/GuessStatisticsMessage.java
@@ -1,4 +1,36 @@
 package glava2.soderzhatelnieimena;
 
 public class GuessStatisticsMessage {
+    private String number;
+    private String verb;
+    private String pluralModifier;
+
+    public String make(String candidate, int count) {
+        createPluralDependentMessageParts(count);
+        return String.format("There %s %s %s%s", verb, number, candidate, pluralModifier);
+    }
+    private void createPluralDependentMessageParts(int count) {
+        if (count == 0) {
+            thereAreNoLetters();
+        } else if(count == 1) {
+            thereIsOneLetter();
+        } else {
+            thereAreManyLetters(count);
+        }
+    }
+    private void thereAreManyLetters(int count) {
+        number = Integer.toString(count);
+        verb = "are";
+        pluralModifier = "s";
+    }
+    private void thereIsOneLetter() {
+        number = "1";
+        verb = "is";
+        pluralModifier = "";
+    }
+    private void thereAreNoLetters() {
+        number = "no";
+        verb = "are";
+        pluralModifier = "s";
+    }
 }
--- CleanCode/src/test/java/glava2/soderzhatelnieimena/GuessStatisticsMessageTest.java
+++ CleanCode/src/test/java/glava2/soderzhatelnieimena/GuessStatisticsMessageTest.java
@@ -1,4 +1,12 @@
 package glava2.soderzhatelnieimena;
+import static org.hamcrest.core.Is.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class GuessStatisticsMessageTest {
+    @Test
+    public void when2cakesWeHave() {
+        GuessStatisticsMessage cakeOrderMessage = new GuessStatisticsMessage();
+        assertThat(cakeOrderMessage.make("cake", 2), is("There are 2 cakes"));
+    }
 }
